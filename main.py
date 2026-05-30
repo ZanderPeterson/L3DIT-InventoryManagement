3fb2759d3dfd964880fc249ae4142fbe4ca71459
Zander Peterson <zander.j.peterson@gmail.com>
2026-05-30 16:07:21 +1200

    Added Title

--- main.py
+++ main.py
@@ -15,6 +15,21 @@ class Widget(QWidget):
         self.setWindowTitle("Havelock North Scout Hall — Inventory Management") #Sets the title of the window
         self.resize(800, 600)
 
+        main_columns = QHBoxLayout()
+        left_side = QVBoxLayout()
+        right_side = QVBoxLayout()
+
+        #Set up title
+        title_label = QLabel("<h1>Inventory Management</h1>") #Creates a heading for the output section
+        title_label.setStyleSheet("background-color: #cccccc; color: black; padding: 10px; border: 2px solid black; border-radius: 16px;")
+        left_side.addWidget(title_label, alignment=Qt.AlignCenter)
+        left_side.addStretch()
+
+        #Adds both of the sides to the overall layout
+        main_columns.addLayout(left_side)
+        main_columns.addLayout(right_side)
+        self.setLayout(main_columns) #Displays layout
+
 app = QApplication(sys.argv)
 print("running...")
 
