3fcc4d4fc174905d751845acb8a9f166b1703a87
Zander Peterson <zander.j.peterson@gmail.com>
2026-06-02 19:13:12 +1200

    1.1.1 — Create the basic element
    Done by creating a "basic_element" string with QSS, that can be used whenever constructing future elements.

--- main.py
+++ main.py
@@ -15,13 +15,21 @@ class Widget(QWidget):
         self.setWindowTitle("Havelock North Scout Hall — Inventory Management") #Sets the title of the window
         self.resize(800, 600)
 
+        #QSS (Effectively CSS)
+        basic_element: str = """background-color: #cccccc; 
+                                color: black; 
+                                padding: 10px; 
+                                border: 2px solid black; 
+                                border-radius: 16px;"""
+
+        #Layouts
         main_columns = QHBoxLayout()
         left_side = QVBoxLayout()
         right_side = QVBoxLayout()
 
         #Set up title
         title_label = QLabel("<h1>Inventory Management</h1>") #Creates a heading for the output section
-        title_label.setStyleSheet("background-color: #cccccc; color: black; padding: 10px; border: 2px solid black; border-radius: 16px;")
+        title_label.setStyleSheet(basic_element)
         left_side.addWidget(title_label, alignment=Qt.AlignCenter)
         left_side.addStretch()
 
