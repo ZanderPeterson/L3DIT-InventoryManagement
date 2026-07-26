b107e413ae8e80c4e7d042f10509159ef51e0bc4
Zander Peterson <zander.j.peterson@gmail.com>
2026-07-27 10:26:56 +1200

    Added Functionality To Fill UUIDs
    If there are cells in items.csv in the UUID column that are empty, newly written UUIDs (using uuid4) are written.
    This is implemented as a new utility function within the local-data/data-utils.py file.

--- local-data/data-utils.py
+++ local-data/data-utils.py
@@ -1,11 +1,33 @@
 #This file will contain functions to help manage data.
 
 #Stardard Libraries
+import csv
+import uuid
 
 #Third-Party Libraries
 
 #Local Libraries
 
 #File Names
-item_list: str = "items.csv"
+item_csv: str = "items.csv"
 
+def add_uuids_to_items():
+    """
+    Checks through items.csv and finds any missing UUIDs (These may be present
+    if the user has just manually added new rows). If missing UUIDs are found,
+    the empty cells are replaced with newly generated UUIDs (using uuid.uuid4)
+    """
+    with open(item_csv, mode="r", newline="") as csv_file:
+        read_file = list(csv.reader(csv_file))
+        for row in read_file:
+            if row[0] == '':
+                row[0] = uuid.uuid4() #Adds a UUID, if no UUID is present
+                print(f"Added UUID to {row}")
+
+    with open(item_csv, mode="w", newline="") as csv_file:
+        csv.writer(csv_file).writerows(read_file) #Writes file w/changes.
+
+if __name__ == "__main__":
+    print("Warning: File is intended to be used for utilities rather than run directly.")
+    print("main.py is the intended file for this code to be ran from.")
+    add_uuids_to_items()
