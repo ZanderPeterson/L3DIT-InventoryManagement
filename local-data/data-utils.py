9a2d2525244531df4f6541dbd7eb100ac72e2556
Zander Peterson <zander.j.peterson@gmail.com>
2026-07-27 13:47:26 +1200

    Added Function to Return Item Information

--- local-data/data-utils.py
+++ local-data/data-utils.py
@@ -27,7 +27,23 @@ def add_uuids_to_items():
     with open(item_csv, mode="w", newline="") as csv_file:
         csv.writer(csv_file).writerows(read_file) #Writes file w/changes.
 
+def get_item_information(uuid:str) -> dict[str, str]:
+    """
+    A function that returns the basic information of an item, given the UUID.
+    This currently gives the Name of the item & the item description,
+    but will later give availability, number of issues, etc.
+    """
+    item_info: dict[str, str] = {}
+    with open(item_csv, mode="r", newline="") as csv_file:
+        read_file = list(csv.reader(csv_file))
+        for item in read_file:
+            if item[0] == uuid:
+                item_info["name"] = item[1]
+                item_info["description"] = item[2]
+    return item_info
+
 if __name__ == "__main__":
     print("Warning: File is intended to be used for utilities rather than run directly.")
     print("main.py is the intended file for this code to be ran from.")
     add_uuids_to_items()
+    print(get_item_information("4e3d4cfc-1e45-42c3-8052-c3b356f12153"))
